b2b65d16bbdc3443e24762ebe11cfabfbecd8333
MahmoudSami12 <ms4805727@gmail.com>
2023-04-15 23:04:20 +0200

    Added Calculator

---
+++ main.js
@@ -0,0 +1,21 @@
+let screen = document.getElementById("screen");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "C":
+        screen.innerText = "";
+        break;
+      case "←":
+        screen.innerText = screen.innerText.slice(0, -1);
+        break;
+      case "=":
+        screen.innerText = eval(screen.innerText);
+        break;
+      default:
+        screen.innerText += button.innerText;
+    }
+  });
+});
